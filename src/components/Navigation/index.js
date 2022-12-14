4c2dfad9317ccf8fafbd18c722a106308c02a89f
Brian Hoang <brianhoang36@gmail.com>
2022-12-14 14:06:13 -0800

    more src components, need to fix header and resume

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+
+function Navigation(props) {
+    return (
+        <div className=" row navigation " id="navigation">
+            <NavLink to="/about"> </NavLink>
+            <NavLink to="/contact"> </NavLink>
+            <NavLink to="/portfolio"> </NavLink>
+            <NavLink to="/resume"> </NavLink>
+        </div>
+    )
+}
+
+export default Navigation;
---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ProjectCards from '../../components/ProjectCards';
+import portfolio from '../../portfolio.json';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Wrapper(props) {
+  return <div className="wrapper">{props.children}</div>;
+}
+
+function Portfolio() {
+
+    return (
+        <section>
+            <div  className="project">
+                <h1 className='title'> Projects Portfolio: brianthoang </h1>
+                <hr></hr>
+            </div>
+
+            <Wrapper id="card-data">
+                {portfolio.map((project) => (
+                    <ProjectCards key={project.id} images={project.images} title={project.title} github={project.github} deployed={project.deployed} />
+                ))}
+            </Wrapper>
+        </section>
+    )
+}
+
+export default Portfolio;
